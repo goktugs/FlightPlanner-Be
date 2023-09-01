a9da60b332f2678cf425fb33bdb8ee4a9f05853e
goktugs <gokojapan5@gmail.com>
2023-09-01 23:49:03 +0300

    feat: add getFlights

--- src/controllers/root/postGetFlights.js
+++ src/controllers/root/postGetFlights.js
@@ -1,3 +1,4 @@
+const localData = require("../../../allFlights.json");
 /**
  * Echo endpoint
  * @param {import('express').Request} req
@@ -5,7 +6,15 @@
  */
 const postGetFlights = (req, res) => {
   const json = req.body;
-  res.json(json);
+
+  const filteredData = localData.filter((flight) => {
+    return (
+      flight.departure_airport === json.depAirport.airport_name &&
+      flight.arrival_airport === json.arrAirport.airport_name
+    );
+  });
+
+  res.json(filteredData);
 };
 
 export default postGetFlights;
