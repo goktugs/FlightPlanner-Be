1fe887f34aa9eaab545ba890c263ace4a4a004ba
goktugs <gokojapan5@gmail.com>
2023-09-03 13:45:37 +0300

    fix: cors

--- src/config.js
+++ src/config.js
@@ -1,22 +1,22 @@
-import packageJson from '../package.json';
+import packageJson from "../package.json";
 
 /**
  * Pattern for config is:
  * key: process.env['KEY'] ?? default
  */
 const config = {
-    version: packageJson.version,
-    name: packageJson.name,
-    description: packageJson.description,
+  version: packageJson.version,
+  name: packageJson.name,
+  description: packageJson.description,
 
-    nodeEnv: process.env['NODE_ENV'] ?? 'development',
-    port: process.env['PORT'] ?? 3000,
+  nodeEnv: process.env["NODE_ENV"] ?? "development",
+  port: process.env["PORT"] ?? 3000,
 
-    clientOrigins: {
-        'test': process.env['DEV_ORIGIN'] ?? '*',
-        'development': process.env['DEV_ORIGIN'] ?? '*',
-        'production': process.env['PROD_ORIGIN'] ?? 'none'
-    }
-}
+  clientOrigins: {
+    test: process.env["DEV_ORIGIN"] ?? "*",
+    development: process.env["DEV_ORIGIN"] ?? "*",
+    production: process.env["PROD_ORIGIN"] ?? "*",
+  },
+};
 
-export default config
+export default config;
